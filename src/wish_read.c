7e9e6bab85c3a4db9d3587a6bf2e886de269307a
Jthomas62270 <Jthomas62270@gmail.com>
2023-02-09 13:28:00 -0500

    Commit #1.4.2:HW#1 - Feb. 9, 2023 -removed the loop on the getline function -copying string from buffer to line without newline key

--- src/wish_read.c
+++ src/wish_read.c
@@ -5,109 +5,47 @@
 char *wish_read_line(FILE *in) { 
   
   if(in == NULL){
-    perror("No line is present..."); 
+    perror("No file is present..."); 
     return NULL; 
   }
 
+  //declaration 
   char* buff[WISH_MAX_INPUT];
-  size_t buff_len = sizeof(buff); 
-  char* line = malloc(buff_len); 
+  size_t len = sizeof(buff); 
+  char* line = malloc(len); 
 
   line[0] = '\0'; 
+  
+  // readline and save it to buffer
+  // param: destination, length of the input, file to stream
+  size_t read = getline(buff, &len, in); 
 
-  if(line == NULL){
-    perror("Unable to allocate memory...\n"); 
+  // size_t return -1 if there is issue 
+  if(read == (size_t)-1){
+    perror("Error reading message...\n"); 
     free(line); 
     return NULL; 
   }
 
-  size_t len = getline(buff,&buff_len, in);  
-
-  if(len > WISH_MAX_INPUT){ 
-    perror("wish : line too long"); 
+  // if read == 0 nothing on the line is present 
+  if(read == 0){ 
+    free(line); 
+    return NULL; 
+  }
+  // if it stream more than the amount of memory available
+  if(read > sizeof(line)){
+    fprintf(stderr,"wish : line too long\n");
+    free(line); 
     return NULL; 
   }
 
-  if(len == 0){ 
-    perror("Buffer is empty"); 
-    return NULL; 
-  }
+  // copies from buffer to a new array without newline key
+  strncpy(line, *buff, read-1); 
+  line[read] = '\0';
+  printf("%s\n", line); 
 
-  strncpy(line, *buff, len-1); 
-  printf("%s\n",line);
-  return line; 
+  return line;  
 }
-  /*
-  if (in == NULL){
-    perror("No line is present..."); 
-    return NULL; 
-  }
-
-  char chunk[WISH_MAX_INPUT]; 
-  
-  size_t len = sizeof(chunk); 
-  char *line = malloc(len); 
-
-  if(line == NULL){
-    perror("Unable to allocate memory for line buffer..."); 
-    exit(1); 
-  }
-
-  line[0] = '\0'; 
-
-  while(fgets(chunk, sizeof(chunk), in) != NULL){
-    size_t len_used = strlen(line); 
-    size_t chunk_used = strlen(chunk); 
-
-    if(len - len_used < chunk_used){
-      len *= 2; 
-      fprintf(stderr, "wish : line too long\n"); 
-      free(line); 
-      return NULL; 
-    }
-
-    strncpy(line + len_used, chunk, len-len_used); 
-    len_used += chunk_used; 
-
-     if(*line == '\0'){
-        return NULL; 
-    } 
-
-    if(line[len_used-1] == '\n'){
-      fputs(line, stdout); 
-      len_used -= 1;  
-      return line;
-    }
-  }
-  return NULL; 
-}
-*/
-
-  /*
-  if(read == -1){
-    perror(1); 
-    return NULL; 
-  }
-
-  if(ptr[read-1]== '\n'){
-    ptr[read-1] = '\0'; 
-  }
-
-  if(read > WISH_MAX_INPUT){
-    free(ptr); 
-    return NULL; 
-  }
-
-  for(int i = 0; i < read - 1; i++){
-    if(isspace(ptr[i])){
-      return ptr; 
-    }
-  }
-
-  free(ptr); 
-  return NULL; 
-  */
-
 
 int wish_read_config(char *fname, int ok_if_missing) {
 
