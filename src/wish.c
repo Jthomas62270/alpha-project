43cc06fd96aaa43d0f95e99daf5d6d7372007694
Tubby-john <91159100+Tubby-john@users.noreply.github.com>
2023-02-09 22:12:42 -0500

    Commit #1.5:HW#1 Feb. 9, 2023 -Need to add Loop iteration so more data can be seen

--- src/wish.c
+++ src/wish.c
@@ -1,18 +1,14 @@
 #include "wish.h"
 
-int main(int argc, char *argv[])
+int main()
 {
   // These two lines make the macOS C compiler happy.
   // Otherwise, it complains about unused parameters.
-  argc = 0;
-  argv = NULL;
-  
+
   // This is just a skeleton for your convenience
-  printf(WISH_DEFAULT_PROMPT); 
+  printf(WISH_DEFAULT_PROMPT);
   wish_read_line(stdin);
-  char *file = getenv("$HOME"); 
-  wish_read_config(file, 1); 
-  wish_read_config("wish.config", 1);
-  
+  wish_read_config("wish.config", 0);
+
   return EXIT_SUCCESS;
 }
--- src/wish_read.c
+++ src/wish_read.c
@@ -2,65 +2,58 @@
 #include <string.h>
 #include <ctype.h>
 
-char *wish_read_line(FILE *in) { 
-  
-  if(in == NULL){
-    perror("No file is present..."); 
-    return NULL; 
+char *wish_read_line(FILE *in)
+{
+
+  if (in == NULL)
+  {
+    perror("No file is present...");
+    return NULL;
   }
 
-  //declaration 
-  char* buff[WISH_MAX_INPUT];
-  size_t len = sizeof(buff); 
-  char* line = malloc(len); 
-  //printf("size of buff: %ld", sizeof(buff)); 
-  //printf("size of line: %ld", sizeof(line)); 
-
-  line[0] = '\0'; 
+  // declaration
+  char *buff = NULL;
+  // char *line[WISH_MAX_INPUT];
+  size_t len = WISH_MAX_INPUT;
+  size_t buff_used;
 
   // readline and save it to buffer
   // param: destination, length of the input, file to stream
-  size_t read = getline(buff, &len, in); 
+  if ((buff_used = getline(&buff, &len, in)) != 1)
+  {
+    if (buff_used == 0)
+    {
+      perror("No line entered...\n");
+      return NULL;
+    }
 
-  // size_t return -1 if there is issue 
-  if(read == (size_t)-1){
-    perror("Error reading message...\n");  
-    free(line); 
-    return NULL; 
+    if (buff_used > WISH_MAX_INPUT)
+    {
+      fprintf(stderr, "wish : line too long");
+      free(buff);
+      return NULL;
+    }
+    buff[buff_used - 1] = '\0';
   }
-
-  // if read == 0 nothing on the line is present 
-  if(read == 0){ 
-    free(line); 
-    return NULL; 
-  }
-  // if it stream more than the amount of memory available
-  //printf("%ld, %ld", read, sizeof(line)); 
-  if(read > len){
-    fprintf(stderr,"wish : line too long\n");
-    free(line); 
-    return NULL; 
-  }
-
-  // copies from buffer to a new array without newline key
-  strncpy(line, *buff, read-1); 
-  if(line[read-1] == '\n'){
-    line[read-1] = '\0';
-  }
-  printf("%s\n", line); 
-
-  return line;  
+  printf("%s\n", buff);
+  // free(buff);
+  return buff;
 }
 
-int wish_read_config(char *fname, int ok_if_missing) {
+int wish_read_config(char *fname, int ok_if_missing)
+{
 
-  if((ok_if_missing == 0) && (fname != NULL)){
-    FILE *in = fopen(fname, "r"); 
+  if ((ok_if_missing == 0) && (fname != NULL))
+  {
+    FILE *in = fopen(fname, "r");
     char *line = wish_read_line(in);
-  } else if((ok_if_missing == 0) && (fname == NULL)) { 
-    perror("File not found...\n"); 
-    return 1;  
-  } 
+    fclose(in);
+  }
+  else if ((ok_if_missing == 0) && (fname == NULL))
+  {
+    perror("File not found...\n");
+    return 1;
+  }
 
   return 0;
 }
